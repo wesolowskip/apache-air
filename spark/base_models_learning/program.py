94641e601868743fe065bd0f88cea7ac728b0629
luki139 <luki139@gmail.com>
2023-01-15 01:50:29 +0100

    Dobra, to powinno totalnie być okej

--- spark/base_models_learning/program.py
+++ spark/base_models_learning/program.py
@@ -5,6 +5,7 @@ from pyspark.sql.functions import expr, from_json, col, concat, round, lit, year
 from pyspark.streaming import StreamingContext
 from pyspark.ml.regression import GBTRegressionModel
 from pyspark.ml.feature import VectorAssembler
+from pyspark.ml import Pipeline
 
 import os
 print(os.getcwd(), 'PAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAtth')
@@ -85,15 +86,15 @@ value_df_weather = df_weather.selectExpr("CAST(key AS STRING)", "CAST(value AS S
 json_df_weather = value_df_weather.select(from_json(col("value"),schema_weather).alias("value"), 'timestamp')
 exploaded_df_weather = json_df_weather.select("value.*", "timestamp")
 
-exploaded_df_weather = exploaded_df_weather.withColumn("lon", round("`coord_lon`", 4))
-exploaded_df_weather = exploaded_df_weather.withColumn("lat", round("`coord_lat`", 4))
+exploaded_df_weather = exploaded_df_weather.withColumn("lon", round("coord_lon", 4))
+exploaded_df_weather = exploaded_df_weather.withColumn("lat", round("coord_lat", 4))
 watermarked_weather = exploaded_df_weather.withWatermark("timestamp", "1 minute")
 
 value_df_air = df_air.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)")
 json_df_air = value_df_air.select(from_json(col("value"),schema_air).alias("value"), "timestamp")
 exploaded_df_air = json_df_air.select("value.*", "timestamp")
-exploaded_df_air = exploaded_df_air.withColumn("lon", round("`station_gegrLon`", 4))
-exploaded_df_air = exploaded_df_air.withColumn("lat", round("`station_gegrLat`", 4))
+exploaded_df_air = exploaded_df_air.withColumn("lon", round("station_gegrLon", 4))
+exploaded_df_air = exploaded_df_air.withColumn("lat", round("station_gegrLat", 4))
 watermarked_air = exploaded_df_air.withWatermark("timestamp", "1 minute")
 
 joined_df = watermarked_air.alias("air").join(watermarked_weather.alias("weather"), expr("""
@@ -101,31 +102,30 @@ joined_df = watermarked_air.alias("air").join(watermarked_weather.alias("weather
     air.lat = weather.lat
 """))
 
-no2_table = joined_df.filter("`sensor_param_code` = 'NO2'").select("*", month("`measurement_date`").alias("month"),hour("`measurement_date`").alias("hour"), dayofweek("`measurement_date`").alias("weekday"))
-o3_table = joined_df.filter("`sensor_param_code` = '03'").select("*", month("`measurement_date`").alias("month"),hour("`measurement_date`").alias("hour"), dayofweek("`measurement_date`").alias("weekday"))
-pm25_table = joined_df.filter("`sensor_param_code` = 'PM10'").select("*", month("`measurement_date`").alias("month"),hour("`measurement_date`").alias("hour"), dayofweek("`measurement_date`").alias("weekday"))
-pm10_table = joined_df.filter("`sensor_param_code` = 'PM25'").select("*", month("`measurement_date`").alias("month"),hour("`measurement_date`").alias("hour"), dayofweek("`measurement_date`").alias("weekday"))
-# no2_table_out = no2_table \
-#     .writeStream \
-#     .outputMode("append") \
-#     .format("console") \
-#     .start()
+no2_table = joined_df.filter("sensor_param_code = 'NO2'").select("*", month("measurement_date").alias("month"),hour("measurement_date").alias("hour"), dayofweek("measurement_date").alias("weekday"))
+o3_table = joined_df.filter("sensor_param_code = '03'").select("*", month("measurement_date").alias("month"),hour("measurement_date").alias("hour"), dayofweek("measurement_date").alias("weekday"))
+pm25_table = joined_df.filter("sensor_param_code = 'PM10'").select("*", month("measurement_date").alias("month"),hour("measurement_date").alias("hour"), dayofweek("measurement_date").alias("weekday"))
+pm10_table = joined_df.filter("sensor_param_code = 'PM25'").select("*", month("measurement_date").alias("month"),hour("measurement_date").alias("hour"), dayofweek("measurement_date").alias("weekday"))
+
 # no2_table_out.awaitTermination()
-modeln02 = GBTRegressionModel.load("/home/base_models_learning/2022_12_19_15_14_46_NO2.model")
-modelo3 = GBTRegressionModel.load("/home/base_models_learning/2022_12_19_15_14_46_O3.model")
-modelpm10 = GBTRegressionModel.load("/home/base_models_learning/2022_12_19_15_14_46_PM10.model")
-modelpm25 = GBTRegressionModel.load("/home/base_models_learning/2022_12_19_15_14_46_PM25.model")
-print(modeln02.columns)
+modeln02 = GBTRegressionModel.load("/home/base_models_learning/2022_12_17_03_58_15_NO2.model")
+modelo3 = GBTRegressionModel.load("/home/base_models_learning/2022_12_17_03_58_15_O3.model")
+modelpm10 = GBTRegressionModel.load("/home/base_models_learning/2022_12_17_03_58_15_PM10.model")
+modelpm25 = GBTRegressionModel.load("/home/base_models_learning/2022_12_17_03_58_15_PM25.model")
+# print(modeln02.columns)
 
-assembler = VectorAssembler(inputCols=["`measurement_value`", "`main_temp`", "`main_pressure`", "`main_humidity`", "`wind_speed`", "`wind_deg`", "`clouds_all`", "snow_1h", "rain1_h", "month", "hour", "weekday"], outputCol="features")
-assembled_df_n02 = assembler.transform(no2_table)
-assembled_df_o3 = assembler.transform(o3_table)
-assembled_df_pm25 = assembler.transform(pm25_table)
-assembled_df_pm10 = assembler.transform(pm10_table)
+assembler = VectorAssembler(inputCols=["measurement_value", "main_temp", "main_pressure", "main_humidity", "wind_speed", "wind_deg", "clouds_all", "snow_1h", "rain_1h", "month", "hour", "weekday"], outputCol="features")
+final_df_n02 = assembler.transform(no2_table)
+final_df_o3 = assembler.transform(o3_table)
+final_df_pm25 = assembler.transform(pm25_table)
+final_df_pm10 = assembler.transform(pm10_table)
 
 
-print(modeln02.predict(assembled_df_n02))
-print(modelo3.predict(assembled_df_o3))
-print(modelpm25.predict(assembled_df_pm25))
-print(modelpm10.predict(assembled_df_pm10))
+no2_table_out = modeln02.transform(final_df_n02).writeStream.outputMode("append").format("console").start().awaitTermination()
+o3_table_out = modelo3.transform(final_df_o3).writeStream.outputMode("append").format("console").start().awaitTermination()
+pm25_table_out = modelpm25.transform(final_df_pm25).writeStream.outputMode("append").format("console").start().awaitTermination()
+pm10_table_out = modelpm10.transform(final_df_pm10).writeStream.outputMode("append").format("console").start().awaitTermination()
+
+
+
 # ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1 --master spark://spark-master:7077 /home/base_models_learning/program.py
