8b4d608671fab40e6126d776a3ae44c089ca5ed7
Krystian Kurek <kszkurek@gmail.com>
2023-01-16 16:49:01 +0100

    'namenode'

---
+++ hadoop/namenode/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+hdfs dfs -test -d /summary
+if [ $? -eq 1 ]; then
+    echo "DIR DO NOT EXIST"
+    hdfs dfs -mkdir /summary
+    echo "DIR CREATED"
+fi
+hdfs dfs -test -e /complete_data.csv
+if [ $? -eq 1 ]; then
+    echo "complete_data.csv do not exists, coping..."
+    hdfs dfs -put /home/complete_data.csv /complete_data.csv
+fi
+
